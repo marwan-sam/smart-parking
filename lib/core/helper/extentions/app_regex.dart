604684502dcccabaaa144aa83254ff30b4f32348
marwan-sam <marwan.elansarey@gmail.com>
2024-06-24 21:15:46 +0200

    add extention

---
+++ lib/core/helper/extentions/app_regex.dart
@@ -0,0 +1,48 @@
+class AppRegex {
+  static bool isEmailValid(String email) {
+    return RegExp(r'^.+@[a-zA-Z]+\.{1}[a-zA-Z]+(\.{0,1}[a-zA-Z]+)$')
+        .hasMatch(email);
+  }
+
+  static bool isPasswordValid(String password) {
+    return RegExp(
+            r"^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$")
+        .hasMatch(password);
+  }
+
+  static bool isPhoneNumberValid(String phoneNumber) {
+    return RegExp(r'^(010|011|012|015)[0-9]{8}$').hasMatch(phoneNumber);
+  }
+
+  static bool hasLowerCase(String password) {
+    return RegExp(r'^(?=.*[a-z])').hasMatch(password);
+  }
+
+  static bool hasUpperCase(String password) {
+    return RegExp(r'^(?=.*[A-Z])').hasMatch(password);
+  }
+
+  static bool hasNumber(String password) {
+    return RegExp(r'^(?=.*?[0-9])').hasMatch(password);
+  }
+
+  static bool hasSpecialCharacter(String password) {
+    return RegExp(r'^(?=.*?[#?!@$%^&*-])').hasMatch(password);
+  }
+
+  static bool hasMinLength(String password) {
+    return RegExp(r'^(?=.{8,})').hasMatch(password);
+  }
+}
+// class AppRegExp {
+//   AppRegExp._();
+
+//   static final numericRegex = RegExp(r'[0-9]');
+//   static final symRegex = RegExp(r'[!@#$%^&*()]');
+//   static final hasUpperCase = RegExp(r'[A-Z]');
+//   static final hasLowerCase = RegExp(r'[a-z]');
+// }
+
+extension StringExtension on String? {
+  bool isNullOrEmpty() => this == null || this == "";
+}
