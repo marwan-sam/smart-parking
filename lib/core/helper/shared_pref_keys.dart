b5d7a1df09e7dcb626515e045b152ca33919f489
marwan-sam <marwan.elansarey@gmail.com>
2024-06-24 20:08:54 +0200

    add init class shared pref.

---
+++ lib/core/helper/shared_pref_keys.dart
@@ -0,0 +1,15 @@
+class SharedPrefKeys {
+  static const String userToken = 'userToken';
+  static const String userEmail = 'userEmail';
+  static const String userName = 'userName';
+  static const String ticketTimeHOUT = 'ticket_hour_out';
+  static const String ticketTimeHIN = 'ticket_hour_in';
+  static const String ticketTimeDay = 'ticket_day';
+  static const String ticketTimeMOUT = 'ticket_min_out';
+  static const String ticketTimeMIN = 'ticket_min_in';
+  static const String ticketTimeMonth = 'ticket_month';
+  static const String getPrice = 'price';
+  // static const String ticket = 'ticket';
+}
+
+bool isLoggedInUser = false;
